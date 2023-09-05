ddfaa24ca6c9be14f38dcc93235772e555de2c87
Lukas Schmid <dimschlukas@gmail.com>
2023-09-05 19:13:08 +0200

    CLI: Add config command

--- webling_calendar/cli.py
+++ webling_calendar/cli.py
@@ -1,6 +1,7 @@
 import click
+from webling_calendar.config import Config
 
-# from dotenv import set_key
+CONFIG = Config()
 
 
 @click.group()
@@ -13,9 +14,24 @@ def main():  # pragma: no cover
     pass
 
 
-@main.command()
-@click.option("--set")
-@click.option("api.key", prompt=True)
-def config(api_key):  # pragma: no cover
-    click.echo(f"set API Key: {api_key}")
-    # set_key('./.env', 'WEBLING_API_KEY', api_key)
+@main.command(no_args_is_help=True)
+@click.option("--set-api-key", help="Sets and saves API key")
+@click.option("--set-api-url", help="Sets and saves API enpoint url.")
+@click.option("--get-api-key", help="Show API key", is_flag=True)
+@click.option("--get-api-url", help="Show API url.", is_flag=True)
+def config(
+    set_api_key, set_api_url, get_api_key, get_api_url
+):  # pragma: no cover
+    if get_api_key:
+        click.echo(f"API Key: {CONFIG.webling_api_key}")
+    if get_api_url:
+        click.echo(f"API endpoint url: {CONFIG.webling_endpoint_url}")
+    if set_api_key:
+        CONFIG.set_api_key(set_api_key)
+    if set_api_url:
+        CONFIG.set_enpoint_url(set_api_url)
+
+    try:
+        CONFIG.check_configuration()
+    except Exception as e:
+        click.secho(e, fg="red", err=True)
